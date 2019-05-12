1f65f366ffd3394f6789b175f3cb95fcc6a56270
Tim Mein <mebusmein@gmail.com>
2019-05-12 19:17:51 +0200

    add url via extension and add filter for empty ordres

--- src/index.js
+++ src/index.js
@@ -1,63 +1,70 @@
-require('dotenv').config()
-const { ApolloServer, gql } = require('apollo-server')
-const { importSchema } = require('graphql-import')
-const { Prisma } = require('prisma-binding')
-const path = require('path')
-const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: process.env.ELASTICSEARCH_HOST || 'http://localhost:9200'  })
-const fs = require('fs')
-const Thumbor = require('thumbor')
+require("dotenv").config();
+const { ApolloServer, gql } = require("apollo-server");
+const { importSchema } = require("graphql-import");
+const { Prisma } = require("prisma-binding");
+const path = require("path");
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({
+  node: process.env.ELASTICSEARCH_HOST || "http://localhost:9200"
+});
+const fs = require("fs");
+const Thumbor = require("thumbor");
 
-const bucketdir = '/data/bucket/'
-const printdir = '/data/print/'
+const bucketdir = "/data/bucket/";
+const printdir = "/data/print/";
 
-const thumbor = new Thumbor('', 'http://localhost:8169')
+const thumbor = new Thumbor("", "http://localhost:8169");
 
 const resolvers = {
   Query: {
     orders: (_, args, context, info) => {
-      return context.prisma.query.orders(
-        {
-          where: {
-            status: args.status
-          }
-        },
-        info
-      ).then(x => {
-        return x.map(i => {
-          i.photos = i.photos.map(p => {
-            if(p.url == null){
-                p.url = thumbor.setImagePath(p.tag).resize(150,150).buildUrl();
+      return context.prisma.query
+        .orders(
+          {
+            where: {
+              status: args.status
+            }
+          },
+          info
+        )
+        .then(x => {
+          return x.map(i => {
+            i.photos = i.photos.map(p => {
+              if (p.url == null && p.tag) {
+                p.url = thumbor
+                  .setImagePath(p.tag)
+                  .resize(150, 150)
+                  .buildUrl();
               }
-                
               return p;
-          })
+            });
 
-          return i;
+            return i;
+          });
         });
-      })
     },
     findImage: async (_, args, context, info) => {
-        let result = await client.search({
-          index: 'fotoindex',
-          body: { 
-            query: {
-              wildcard : {
-                filename: "*"+args.tag+"*"
-              }
+      let result = await client.search({
+        index: "fotoindex",
+        body: {
+          query: {
+            wildcard: {
+              filename: "*" + args.tag + "*"
             }
           }
-        
+        }
       });
 
       return result.body.hits.hits.map(x => {
         let result = x._source;
-        if(result.filename){
-          result.url = thumbor.setImagePath(result.filename).resize(150,150).buildUrl(); 
+        if (result.filename) {
+          result.url = thumbor
+            .setImagePath(result.filename)
+            .resize(150, 150)
+            .buildUrl();
         }
-        return result;  
-      }  
-      );
+        return result;
+      });
     }
   },
   Mutation: {
@@ -76,87 +83,99 @@ const resolvers = {
           }
         },
         info
-      )
+      );
     },
     printOrder: async (_, args, context, info) => {
-        //Get order
+      //Get order
 
-        console.log(args.id);
-        let photos = await context.prisma.query.photos(
-          {
-            where: { 
-              order: {
-                  id: args.id
-              }
+      console.log(args.id);
+      let photos = await context.prisma.query.photos(
+        {
+          where: {
+            order: {
+              id: args.id
             }
-          }, 
-          gql`{
-                id
-                tag
-                amount
-                order {
-                  id
-                }
           }
-          `);
-
-        let success = true;
-
-        //Copy the files
-        for(let photo of photos){
-            let path = bucketdir+photo.tag
-            if(fs.existsSync(path)){
-              for(var i = 0; i < photo.amount;i++){
-               fs.copyFileSync(path, printdir + photo.order.id + ' - ' + i + '-' + photo.tag, (err) => {
-                if (err) {
-                    success = false;
-                    context.prisma.mutation.updatePhoto({
-                      data: {
-                        status: 'ERROR',
-                        error: "Image could not be copied"
-                      },
-                      where: {
-                        id: photo.id
-                      }
-                    });
-                };
-              });}
-            } else {
-              context.prisma.mutation.updatePhoto({
-                data: {
-                  status: 'ERROR',
-                  error: "Image could not be found"
-                },
-                where: {
-                  id: photo.id
-                }
-              });
-              success = false;
+        },
+        gql`
+          {
+            id
+            tag
+            amount
+            order {
+              id
             }
-        }
+          }
+        `
+      );
 
-        if(success){
-          return context.prisma.mutation.updateOrder({
+      let success = true;
+
+      //Copy the files
+      for (let photo of photos) {
+        let path = bucketdir + photo.tag;
+        if (fs.existsSync(path)) {
+          for (var i = 0; i < photo.amount; i++) {
+            fs.copyFileSync(
+              path,
+              printdir + photo.order.id + " - " + i + "-" + photo.tag,
+              err => {
+                if (err) {
+                  success = false;
+                  context.prisma.mutation.updatePhoto({
+                    data: {
+                      status: "ERROR",
+                      error: "Image could not be copied"
+                    },
+                    where: {
+                      id: photo.id
+                    }
+                  });
+                }
+              }
+            );
+          }
+        } else {
+          context.prisma.mutation.updatePhoto({
             data: {
-              status: 'DONE',
+              status: "ERROR",
+              error: "Image could not be found"
+            },
+            where: {
+              id: photo.id
+            }
+          });
+          success = false;
+        }
+      }
+
+      if (success) {
+        return context.prisma.mutation.updateOrder(
+          {
+            data: {
+              status: "DONE",
               error: ""
             },
             where: {
               id: args.id
             }
-          }, info);
-        } else {
-          return context.prisma.mutation.updateOrder({
+          },
+          info
+        );
+      } else {
+        return context.prisma.mutation.updateOrder(
+          {
             data: {
-              status: 'ERROR',
+              status: "ERROR",
               error: "Something is wrong in this order"
             },
             where: {
               id: args.id
             }
-          }, info);
-        }
-
+          },
+          info
+        );
+      }
     }
   },
   Node: {
@@ -164,9 +183,9 @@ const resolvers = {
       return null;
     }
   }
-}
+};
 
-const typeDefs = importSchema(path.resolve('src/schema.graphql'))
+const typeDefs = importSchema(path.resolve("src/schema.graphql"));
 
 const server = new ApolloServer({
   typeDefs,
@@ -174,12 +193,12 @@ const server = new ApolloServer({
   context: req => ({
     ...req,
     prisma: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
+      typeDefs: "src/generated/prisma.graphql",
       endpoint: process.env.PRISMA_URL
     })
   })
-})
+});
 
 server.listen({ port: process.env.PORT }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`)
-})
+  console.log(`🚀  Server ready at ${url}`);
+});
